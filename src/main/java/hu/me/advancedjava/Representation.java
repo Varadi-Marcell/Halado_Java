913673c0be1cc2a4c80cac413208ed6ea2163911
Varadi-Marcell <marcibazsi11@gmail.com>
2022-04-08 13:51:31 +0200

    orai szamologep

--- src/main/java/hu/me/advancedjava/Representation.java
+++ src/main/java/hu/me/advancedjava/Representation.java
@@ -5,11 +5,13 @@ public class Representation
     private String[] args;
 
     public Representation(String[] args){
+
         this.args=args;
     }
     
     public static void printResult(int result)
     {
+
         System.out.println("osszeg" + result);
     }
     public Operands getOperands(){
